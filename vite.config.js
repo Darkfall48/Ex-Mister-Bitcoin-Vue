b017c82d00b173d4a134b7210e3316985fdbdcfe
Darkfall48 <sidney@sebban.be>
2023-02-14 17:08:20 +0200

    Fixed: First Publish !

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,12 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// module.exports = {
-//   publicPath: process.env.NODE_ENV === "production" ? "/Ex-Mister-Bitcoin/" : "/",
-// };
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 })
