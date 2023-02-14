39f8b0d78921b9102a4eff546d94e754e236e606
Darkfall48 <sidney@sebban.be>
2023-02-14 17:33:09 +0200

    Fixed: First Publish ! [Now is working!]

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
+//? Libraries
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/Ex-Mister-Bitcoin/',
+  //? For Backend
+  // build: {
+  //   outDir: '../backend/public',
+  //   emptyOutDir: true,
+  //   assestsDir: './dist',
+  // },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
