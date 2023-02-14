3ee735a654d1e61ad0fb92ed5235af44b2aa72e8
Darkfall48 <sidney@sebban.be>
2023-02-14 16:38:28 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "/Ex-Mister-Bitcoin/" : "/",
+};
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
