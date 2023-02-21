ff65876ceb34871899ef49dce85391e095d54a49
Darkfall48 <sidney@sebban.be>
2023-02-22 01:45:52 +0200

    Fixed: Deploy to GitHub Pages!

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/Ex-Mister-Bitcoin/',
+  base: '/Ex-Mister-Bitcoin-Vue/',
   //? For Backend
   // build: {
   //   outDir: '../backend/public',
